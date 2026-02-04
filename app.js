cfccfbcee21baa5cf8782268421c2810c63ce9aa
MillyRoseGunn <m.gunn@gold.ac.uk>
2026-02-04 19:49:28 +0000

    rewritten state.json for lecture

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
